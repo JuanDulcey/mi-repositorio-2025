8b22b56d704b3feb46ab805d4d818fe536722baf
JuanDulcey <juanedulcey05@gmail.com>
2025-10-15 12:28:07 -0500

    Primer commit del portafolio Dulcey

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./mi-repositorio-2025/**/*.{html,js}", // analiza tus archivos HTML y JS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
